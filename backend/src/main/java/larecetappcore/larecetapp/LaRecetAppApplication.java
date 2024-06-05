34f37582b10b4cf13ea2c94456d2f48d96eb2dc6
lg330-ual <lg330@inlumine.ual.es>
2024-06-05 10:49:56 +0200

    Creacion del proyecto Backend y base de datos con docker-compose y Dockerfiles

---
+++ backend/src/main/java/larecetappcore/larecetapp/LaRecetAppApplication.java
@@ -0,0 +1,13 @@
+package larecetappcore.larecetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaRecetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaRecetAppApplication.class, args);
+	}
+
+}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MenuPrincipalComponent },
+];
